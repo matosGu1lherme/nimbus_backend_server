1e50e3f600b7a7d462826924b8f2e4f73448838e
matosGu1lherme <guimatos.com@gmail.com>
2026-01-15 21:47:31 -0300

    acertando permissão de busca de categoria e tipo de produtos

--- src/main/java/com/nimbus/nimbusWebServer/security/configure/SecurityConfiguration.java
+++ src/main/java/com/nimbus/nimbusWebServer/security/configure/SecurityConfiguration.java
@@ -41,11 +41,13 @@ public class SecurityConfiguration {
     };
 
     // Endpoints que requerem autenticação para serem acessados
-    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
-    };
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = { };
 
     // Endpoints que só podem ser acessador por usuários com permissão de cliente
-    public static final String [] ENDPOINTS_CUSTOMER = {};
+    public static final String [] ENDPOINTS_CUSTOMER = {
+            "/categoria/public/buscar_categorias",
+            "/tipo/public/listar_tipos"
+    };
 
     // Endpoints que só podem ser acessador por usuários com permissão de administrador
     public static final String [] ENDPOINTS_ADMIN = {
